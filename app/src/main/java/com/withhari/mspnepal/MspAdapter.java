6ec14bbb96f95b795deb09c1085e5a46e93988fb
harimanok <codescloudofficial@gmail.com>
2016-08-09 23:53:33 +0545

    add colors

--- app/src/main/java/com/withhari/mspnepal/MspAdapter.java
+++ app/src/main/java/com/withhari/mspnepal/MspAdapter.java
@@ -48,8 +48,11 @@ public class MspAdapter extends ArrayAdapter<MSP> {
     }
 
     private Drawable getBackground(MSP msp) {
+        String[] colors = new String[]{"#1abc9c", "#2ecc71", "#3498db", "#9b59b6", "#34495e", "#16a085",
+                "#27ae60", "#2980b9", "#8e44ad", "#2c3e50", "#f1c40f", "#e67e22", "#e74c3c", "#ecf0f1",
+                "#95a5a6", "#f39c12", "#d35400", "#c0392b", "#bdc3c7", "#7f8c8d"};
         GradientDrawable gd = new GradientDrawable();
-        gd.setColor(Color.BLUE);
+        gd.setColor(Color.parseColor(colors[(msp.FullName.length() + msp.College.length()) % colors.length]));
         return gd;
     }
 
