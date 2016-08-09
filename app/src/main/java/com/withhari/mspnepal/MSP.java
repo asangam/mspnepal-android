0dcaed21f59bbfb7d4f2d841184aa55f60884696
harimanok <codescloudofficial@gmail.com>
2016-08-09 22:12:26 +0545

    MSP class to hold MSP info

---
+++ app/src/main/java/com/withhari/mspnepal/MSP.java
@@ -0,0 +1,10 @@
+package com.withhari.mspnepal;
+
+public class MSP {
+    public String FullName, College, Bio;
+    public MSP(String name, String clg, String bio) {
+        FullName = name;
+        College = clg;
+        Bio = bio;
+    }
+}
